a9a96eca178aefd4cacc15d07c4c934ca87676cb
Viswanath Subramani <Viswanath Subramani>
2016-08-01 01:28:05 +0530

    Please enter the commit message for your changes. Lines starting
    Second commit

--- GracefulExceptionHandling/Program.cs
+++ GracefulExceptionHandling/Program.cs
@@ -10,6 +10,28 @@ namespace GracefulExceptionHandling
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Enter your Name");
+            string Name = Console.ReadLine();
+            try
+            {
+                if (Name.Length>10)
+                {
+                    throw new Exception("Length cannot be more than 10");
+                }
+                Console.WriteLine("Enter your age");
+                int Age = int.Parse(Console.ReadLine());
+                Console.WriteLine("Hello " + Name + ", your Age is " + Age);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Incorrect format. Exiting program. . . ");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+            Console.ReadKey();
+            //Program ends here
         }
     }
 }
